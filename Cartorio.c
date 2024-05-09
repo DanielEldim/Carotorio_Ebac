6f82cc8f267cf946624fc45b2faaa66e5f3292df
DanielEldim <168007284+DanielEldim@users.noreply.github.com>
2024-05-09 18:24:46 -0300

    nova opção de fechar programa

--- Cartorio.c
+++ Cartorio.c
@@ -124,7 +124,8 @@ int main()
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n"); //fim do menu
+		printf("\t3 - Deletar nomes\n"); //fim do menu
+		printf("\t4 - Sair\n");
 		printf("Opção:");
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -143,6 +144,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Fechando o programa");
+			return 0;
 			break;
 			
 			default:
